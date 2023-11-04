2c9a6b0bfacf4c1a9ee4b3e97d5e584685feb7f1
MD.Taraq Rahman <taraqr9@gmail.com>
2023-11-04 15:26:53 +0600

    Pinia fundamental chapter 1

---
+++ src/stores/ProductStore.js
@@ -0,0 +1,17 @@
+import { defineStore} from "pinia";
+
+export const useProductStore = defineStore("ProductStore", {
+    // state
+    state: () =>{
+        return {
+            products:[]
+        };
+    },
+    //actions
+    actions:{
+        async fill(){
+            this.products = (await import("@/data/products.json")).default;
+        }
+    }
+    // getters
+})
